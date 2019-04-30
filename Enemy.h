a82de4acb7082d0cbb45f76ee42d4ea74806f680
jonah <jonah.n.rubin@gmail.com>
2019-04-30 16:38:48 -0400

    Enemy

--- Enemy.h
+++ Enemy.h
@@ -20,38 +20,10 @@ public:
     /* Uses OpenGL to draw the obstruction */
     virtual void draw();
 
-    /* Returns true if the coordinate is inside the box */
-    bool isOverlapping(int x, int y) const;
-
-    /* Change color of the box when the user is hovering over it */
-    void hover();
-
-    /* Change color of the box when the user is clicking on it */
-    void pressDown();
-
-    /* Change the color back when the user is not clicking/hovering */
-    void release();
-
     /* Execute whatever the Object is supposed to do */
     virtual void click(std::function<void()> callback);
 
     void moveBox();
-
-    void moveCenter(int newX, int newY);
-
-    class Quad getBox();
-
-    void setNew();
-
-    std::string getLabel();
-
-    void setLabel(std::string message);
-
-    bool wasTouched();
-
-    void contact();
-
-    void resize(unsigned int w, unsigned int h);
 };
 
 #endif //JR_BA_BT_GRAPHICS_ENEMY_H
