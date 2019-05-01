7d72abba3a04c5777b0855f6878e0860d5077100
ChrisSwingle <cswingle@uvm.edu>
2019-04-30 23:38:37 -0400

    Add Scene class

---
+++ Scene.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Christopher on 4/30/2019.
+//
+
+#include "Scene.h"
+
+Scene::Scene(std::vector<Object*> objects) {
+    this->objects = objects;
+}
+
+void Scene::draw() const {
+    for(Object *obj : objects){
+        obj->draw();
+    }
+}
